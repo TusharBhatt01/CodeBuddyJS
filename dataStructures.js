5526914a590ae27c0bf9d2ddfcd20a6a7632bced
Tushar Bhatt <47344834+TusharBhatt01@users.noreply.github.com>
2022-02-22 15:36:27 +0530

    Add files via upload

---
+++ dataStructures.js
@@ -0,0 +1,27 @@
+// level {2}
+// 1.
+
+let array= [1, 3, 4, 6, 7, 8];
+function newArr(num){
+  return num.filter(n=> n%2 !==0);
+}
+console.log(newArr(array));
+
+
+//2.
+
+let string= "Elie";
+let output= string.toUpperCase();
+console.log(output);
+
+
+//3.
+
+var array= [1, 3, 4, 6, 7, 8, 2, 5];
+var maxValue= math.max(...array);
+console.log(maxValue);
+
+
+
+
+
